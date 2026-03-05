c463cc2243f5406dfe564b3fb51aea04d122bce1
DurdonaHazratova <durdonahazratova01@gmail.com>
2026-03-05 22:06:46 +0500

    add railway config

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
